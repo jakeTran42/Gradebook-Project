56bbf95f52027d623a88c60e673ad7a6035b278e
jakeTran42 <jake.tranehs@gmail.com>
2017-10-03 17:04:38 -0700

    Separation

---
+++ Classroom.py
@@ -0,0 +1,8 @@
+import Student
+
+class Classroom(object):
+    def __init__(self, class_name, date_time, assignment):
+        self.class_name = class_name
+        self.date_time = date_time
+        self.assignment = assignment 
+
---
+++ Student.py
@@ -0,0 +1,13 @@
+'''
+Jake Tran
+Initialized Date: 10/03/2017
+CS1
+Gradebook-Project
+'''
+
+class Student(object):
+    def __init__(self, name, assignment):
+        self.name = name
+        self.assignment = assignment
+    
+
